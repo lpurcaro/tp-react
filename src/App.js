fd50ea126fe674e59905090b8dd5a8a3751cff50
lucila.purcaro <lucila.purcaro@despegar.com>
2020-11-21 15:07:52 -0300

    Manejo ruteo en App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
 
-import RoutesMenu from './components/RoutesMenu';
+import {Nav, Navbar} from "react-bootstrap";
+import {Route} from "react-router";
+import Turnos from "./components/Turnos";
+import Pacientes from "./components/Pacientes";
+import Servicios from "./components/Servicios";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <RoutesMenu/>
+      <BrowserRouter>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href='/'>Amigos Peludos</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href='/'>
+                Turnos
+              </Nav.Link>
+              <Nav.Link href='/pacientes'>
+                Pacientes
+              </Nav.Link>
+              <Nav.Link href="/servicios">
+                Servicios
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Route exact path='/' component={Turnos}/>
+        <Route path='/pacientes' component={Pacientes}/>
+        <Route path='/servicios' component={Servicios}/>
+      </BrowserRouter>
     </div>
   );
 }
